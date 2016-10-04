1fbbc1d10e44e6f02f065874830c59ecc58b65df
OliverCulleyDeLange <olly@oliverdelange.co.uk>
2016-10-04 02:38:52 +0100

    Heroku port

--- mvl-server.js
+++ mvl-server.js
@@ -3,6 +3,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var port = process.env.PORT || 3000;
+
 var availableRooms = [];
 var gamePairs = {};
 function printRooms(){
@@ -71,6 +73,6 @@ io.on('connection', function(socket){
   })
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on port ' + port);
 });
